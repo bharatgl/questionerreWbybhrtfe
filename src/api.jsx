d82a0847eb832939aa91cbbaffd94fb51070a5e1
Bharat goyal <bgoyal1471@gmail.com>
2022-08-31 18:32:17 +0530

    udpated_UI

--- src/api.jsx
+++ src/api.jsx
@@ -1,10 +1,11 @@
 import axios from "axios";
 
- const appData = async () => {
-    const url = 'https://questionnere.rohitbahuguna2.repl.co/user/getdata';
+const appData = async () => {
+  const url = "https://questionnerebE-wysa.bharatgoyal1.repl.co/user/getdata";
 
-    const response = await axios.get(url).catch((error) => {return error})
-    return response;
-}
-export default appData; 
-
+  const response = await axios.get(url).catch((error) => {
+    return error;
+  });
+  return response;
+};
+export default appData;
